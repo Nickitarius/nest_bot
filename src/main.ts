c4466c8099e254651686cdeb42897a12dba1f893
Nickitarius <pilnov.nickita@yandex.ru>
2024-08-30 22:41:22 +0800

    meh

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3900);
 }
 bootstrap();
+
+const API_URL = 'localhost';
+const API_PORT = 3901;
+const API_USER = 'usr';
+const API_PASS = 'pass';
+const TOKEN = '7427864021:AAEzpz80ZfPEQnYMkPsKeKzO2O913kl_xGw';
